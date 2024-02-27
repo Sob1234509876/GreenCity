2fbc94b6d82232ac87bd2d2c2d716a50f2105c07
Jacob Zhang <NewGuy3141@163.com>
2024-02-27 20:57:03 +0800

    Terminalv0.2.2

--- Main/Main.java
+++ Main/Main.java
@@ -1,8 +1,10 @@
 package Main;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
@@ -21,9 +23,24 @@ public class Main {
     public static volatile char pressedKey = '\0';
     public static volatile int fontSize = 16;
 
-    public static JTextArea outArea = new JTextArea();
-    public static JTextField point = new JTextField(">>>");
-    public static JTextField cmdArea = new JTextField("⁰");
+    public static JFrame rootFrame = new JFrame("Green City Terminal 0.2.2");
+    public static JPanel cmdPanel = new JPanel(null);
+
+    public static JTextArea outArea = new JTextArea("SMART CITY 0.2.2\n");
+    public static JTextField point = new JTextField(">>>>>>");
+    public static JTextField cmdArea = new JTextField("size\\20");
+    public static JTextField cmdTitle = new JTextField("Command terminal");
+
+    public static JTextField calTitle = new JTextField("Calender");
+    public static JPanel calPanel = new JPanel(null);
+    public static JCalender calender = new JCalender(LocalDateTime.now());
+
+    public static JTextField cloTitle = new JTextField("Clock");
+    public static JPanel cloPanel = new JPanel(null);
+    public static volatile JTextField bigClock = new JTextField(String.format("%02d:%02d:%02d",
+            LocalDateTime.now().getHour(),
+            LocalDateTime.now().getMinute(),
+            LocalDateTime.now().getSecond()));
 
     public static void main(String[] args) throws Exception {
 
@@ -33,7 +50,8 @@ public class Main {
         ImageIcon icon = new ImageIcon(Paths.get(assetPath.toString(), "icon.png").toString());
         ImageIcon joke = new ImageIcon(Paths.get(assetPath.toString(), "Joke.gif").toString());
 
-        JFrame rootFrame = new JFrame("Green City Terminal 0.2");
+        // rootFrame init.
+
         rootFrame.setVisible(true);
         rootFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         rootFrame.setIconImage(icon.getImage());
@@ -42,9 +60,27 @@ public class Main {
         rootFrame.setResizable(false);
         rootFrame.getContentPane().setBackground(Color.BLACK);
 
-        rootFrame.add(outArea);
-        rootFrame.add(point);
-        rootFrame.add(cmdArea);
+        rootFrame.add(cmdPanel);
+        rootFrame.add(cmdTitle);
+
+        rootFrame.add(calTitle);
+        rootFrame.add(calPanel);
+        calPanel.add(calender);
+
+        rootFrame.add(cloTitle);
+        rootFrame.add(cloPanel);
+        cloPanel.add(bigClock);
+
+        // cmdPanel init.
+
+        cmdPanel.setOpaque(true);
+        cmdPanel.setBounds(450, 200, 1100, 600);
+        cmdPanel.setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1, new Color(192, 255, 255)));
+        cmdPanel.setBackground(Color.BLACK);
+
+        cmdPanel.add(outArea);
+        cmdPanel.add(point);
+        cmdPanel.add(cmdArea);
 
         JFrame jokeFrame = new JFrame("Downloading data ...");
         jokeFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
@@ -56,7 +92,7 @@ public class Main {
 
         log("智慧城市启动");
         log("");
-        log(" __________________");
+        log("  __________________");
         log(" /  ________________|");
         log("|  |________________");
         log(" \\________________  \\");
@@ -197,32 +233,113 @@ public class Main {
             }
         }, "thread3");
 
-        thread1.start();
-        thread2.start();
-        thread3.start();
+        thread1.start(); // crappy
+        thread2.start(); // fake
+        thread3.start(); // logs
 
         log("");
         log("Initilizing frame");
 
+        int siz = 24;
+
+        /////////////////////////////////////////// clock///////////////////////////////////////
+
+        Thread clockThread = new Thread(new Runnable() {
+
+            private LocalDateTime n;
+
+            public void run() {
+                while (true) {
+                    n = LocalDateTime.now();
+                    bigClock.setText(String.format("%02d:%02d:%02d", n.getHour(), n.getMinute(), n.getSecond()));
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                    }
+                }
+            }
+
+        }, "clock thread");
+
+        cloTitle.setEditable(false);
+        cloTitle.setBounds(0, 11 * siz + 6, 450, siz);
+        cloTitle.setFont(new Font("宋体", Font.PLAIN, siz));
+        cloTitle.setBackground(new Color(192, 255, 255));
+        cloTitle.setForeground(Color.BLACK);
+
+        cloPanel.setBounds(0, 12 * siz + 6, 450, 3 * siz + 10);
+        cloPanel.setBackground(Color.BLACK);
+        cloPanel.setForeground(new Color(192, 255, 255));
+        cloPanel.setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1, new Color(192, 255, 255)));
+
+        bigClock.setEditable(false);
+        bigClock.setBounds(225 - 6 * siz, 5, 12 * siz, 3 * siz);
+        bigClock.setFont(new Font("coure", Font.PLAIN, 3 * siz));
+        bigClock.setBackground(Color.BLACK);
+        bigClock.setForeground(new Color(192, 255, 255));
+        bigClock.setBorder(null);
+
+        clockThread.start();
+
+        ///////////////////////////////////////// calender//////////////////////////////////////
+
+        calTitle.setEditable(false);
+        calTitle.setBounds(0, 0, 450, siz);
+        calTitle.setFont(new Font("宋体", Font.PLAIN, siz));
+        calTitle.setBackground(new Color(192, 255, 255));
+        calTitle.setForeground(Color.BLACK);
+
+        calPanel.setBounds(0, siz, 450, 10 * siz + 6);
+        calPanel.setBackground(Color.BLACK);
+        calPanel.setForeground(new Color(192, 255, 255));
+        calPanel.setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1, new Color(192, 255, 255)));
+
+        calender.setEditable(false);
+        calender.setBounds(65, 5, siz * 14, 10 * siz);
+        calender.setFont(new Font("宋体", Font.PLAIN, siz));
+        calender.setBackground(Color.BLACK);
+        calender.setForeground(new Color(192, 255, 255));
+        calender.setBorder(null);
+
+        log(LocalDateTime.now().getMonth().getValue());
+
+        //////////////////////////////////////////// cmd////////////////////////////////////////
+
+        // cmdTitle init.
+        cmdTitle.setEditable(false);
+        cmdTitle.setBounds(450, 200, 1100, siz);
+        cmdTitle.setFont(new Font("宋体", Font.PLAIN, siz));
+        /*
+         * cmdTitle.setForeground(new Color(192, 255, 255));
+         * cmdTitle.setBackground(Color.BLACK);
+         * cmdTitle.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, new Color(192,
+         * 255, 255)));
+         */
+        cmdTitle.setBackground(new Color(192, 255, 255));
+        cmdTitle.setForeground(Color.BLACK);
+        cmdTitle.setBorder(null);
+
+        // outArea init.
         outArea.setEditable(false);
-        outArea.setBounds(500, 250, 1000, 484);
-        outArea.setFont(new Font("宋体", Font.PLAIN, 16));
+        outArea.setBounds(50, 50, 1000, siz);
+        outArea.setFont(new Font("宋体", Font.PLAIN, siz));
         outArea.setForeground(new Color(192, 255, 255));
         outArea.setBackground(Color.BLACK);
 
+        // point init.
         point.setEditable(false);
-        point.setBounds(500, 734, 32, 16);
-        point.setFont(new Font("宋体", Font.PLAIN, 16));
+        point.setBounds(50, 550 - siz, 3 * siz, siz);
+        point.setFont(new Font("宋体", Font.PLAIN, siz));
         point.setForeground(new Color(192, 255, 255));
         point.setBackground(Color.BLACK);
         point.setBorder(null);
 
-        cmdArea.setBounds(532, 734, 968, 16);
-        cmdArea.setFont(new Font("宋体", Font.PLAIN, 16));
+        // cmdArea init.
+        cmdArea.setBounds(50 + 3 * siz, 550 - siz, 500 - 3 * siz, siz);
+        cmdArea.setFont(new Font("宋体", Font.PLAIN, siz));
         cmdArea.setForeground(new Color(192, 255, 255));
         cmdArea.setBackground(Color.BLACK);
         cmdArea.setBorder(null);
-
         cmdArea.addKeyListener(new KeyListener() {
             public void keyPressed(KeyEvent k) {
                 if (k.getKeyChar() == '\n') {
@@ -251,10 +368,12 @@ public class Main {
 
         threadCMD.start();
 
+        ////////////////////////////////////////////////////////////////////////////////////////
+
     }
 
-    public static void log(String txt) {
-        System.out.println(String.format("[%s] %s", LocalDateTime.now().toString(), txt));
+    public static void log(Object txt) {
+        System.out.println(String.format("[%s] %s", LocalDateTime.now().toString(), txt.toString()));
     }
 
     public static String command(String cmd) {
@@ -271,12 +390,16 @@ public class Main {
 
                 int siz = Integer.parseInt(txt);
 
-                outArea.setBounds(500, 250, 1000, siz);
+                outArea.setBounds(50, 50, 1000, siz);
                 outArea.setFont(new Font("宋体", Font.PLAIN, siz));
-                point.setBounds(500, 750 - siz, 3 * siz, siz);
+                point.setBounds(50, 550 - siz, 3 * siz, siz);
                 point.setFont(new Font("宋体", Font.PLAIN, siz));
-                cmdArea.setBounds(500 + 3 * siz, 750 - siz, 500 - 3 * siz, siz);
+                cmdArea.setBounds(50 + 3 * siz, 550 - siz, 500 - 3 * siz, siz);
                 cmdArea.setFont(new Font("宋体", Font.PLAIN, siz));
+                // cmdTitle.setBounds(450, 200, 1100, siz);
+                // cmdTitle.setFont(new Font("宋体", Font.PLAIN, siz));
+
+                // log(cmdTitle.getBackground());
 
                 return "";
             } else {
